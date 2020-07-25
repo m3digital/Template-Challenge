977d3dd0997d8fb9148e07f49b34fd801060a9a1
Matt Gennings <mattguy19@gmail.com>
2020-07-25 14:14:05 -0400

    styled the hidden div

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,13 +3,14 @@ document.getElementById('submit').addEventListener('click', function () {
     event.preventDefault();
     console.log("You clicked without jQuery!");
     var box1 = document.getElementById("inlineCheckbox1");
-    var text = document.getElementById("hiddenDiv")
-    console.log(box1)
-    console.log(text)
-    // Display Div if Box is Checked
+    var div1 = document.getElementById("hiddenDiv")
+    // console.log(box1)
+    // console.log(text)
+
+    // Display Div 1 if Box 1 is Checked
     if (box1.checked == true){
-        text.style.display = "block";
+        div1.style.display = "block";
       } else {
-        text.style.display = "none";
+        div1.style.display = "none";
       }
   });

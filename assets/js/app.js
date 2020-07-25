422cf26478553926112b9904996e7399c47cfb0a
Matt Gennings <mattguy19@gmail.com>
2020-07-25 14:29:14 -0400

    working prototype achieved

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,48 @@
+// Global variables
+var box1 = document.getElementById("inlineCheckbox1");
+var box2 = document.getElementById("inlineCheckbox2");
+var box3 = document.getElementById("inlineCheckbox3");
+var div1 = document.getElementById("hiddenDiv1")
+var div2 = document.getElementById("hiddenDiv2")
+var div3 = document.getElementById("hiddenDiv3")
+
 // Click Handler Vanilla JS Method 2
 document.getElementById('submit').addEventListener('click', function () {
     event.preventDefault();
     console.log("You clicked without jQuery!");
-    var box1 = document.getElementById("inlineCheckbox1");
-    var div1 = document.getElementById("hiddenDiv")
-    // console.log(box1)
-    // console.log(text)
-
     // Display Div 1 if Box 1 is Checked
     if (box1.checked == true){
         div1.style.display = "block";
       } else {
         div1.style.display = "none";
       }
+    // Display Div 2 if Box 2 is Checked
+    if (box2.checked == true){
+        div2.style.display = "block";
+      } else {
+        div2.style.display = "none";
+      }
+    // Display Div 3 if Box 3 is Checked
+    if (box3.checked == true){
+        div3.style.display = "block";
+      } else {
+        div3.style.display = "none";
+      }
+  });
+
+//   Remove button functionality
+document.getElementById('remove1').addEventListener('click', function () {
+    event.preventDefault();
+    console.log("You tried to remove this div!");
+    div1.style.display = "none";
+  });
+document.getElementById('remove2').addEventListener('click', function () {
+    event.preventDefault();
+    console.log("You tried to remove this div!");
+    div2.style.display = "none";
+  });
+document.getElementById('remove3').addEventListener('click', function () {
+    event.preventDefault();
+    console.log("You tried to remove this div!");
+    div3.style.display = "none";
   });

cb842113fa741b3d8d74d37a08634861d49a3745
Matt Gennings <mattguy19@gmail.com>
2020-07-25 14:03:04 -0400

    styling hidden div and more js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,3 @@
-// Click Handler Vanilla JS Method 1
-
-// document.addEventListener('click', function (event) {
-
-	// If the clicked element doesn't have the right selector, bail
-	// if (!event.target.matches('.click-me')) return;
-
-	// Don't follow the link
-	// event.preventDefault();
-
-	// Log the clicked element in the console
-// 	console.log(event.target);
-
-// }, false);
-
 // Click Handler Vanilla JS Method 2
 document.getElementById('submit').addEventListener('click', function () {
     event.preventDefault();
